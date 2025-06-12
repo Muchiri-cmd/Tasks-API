c2586599c47ddc18bb38d397b0cf329140106fba
Muchiri-cmd <davismuchiri21@gmail.com>
2025-06-12 16:12:00 +0300

    Add Read - (get 1 task) Operation

--- index.js
+++ index.js
@@ -40,6 +40,23 @@ app.get("/tasks",async(req,res) => {
     }
 })
 
+app.get("/tasks/:id", async (req, res) => {
+    try{
+        console.log(req.params)
+        const {id} = req.params;
+        const task = await client.task.findFirst({
+            where:
+            {
+                id //: id
+            }
+        })
+        task ? res.status(200).json(task) : res.status(404).json({ message: "Task not found." });
+    }catch(error){
+        res.status(500).json({ message: "Error fetching task, please try again later." });
+    }
+   
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
