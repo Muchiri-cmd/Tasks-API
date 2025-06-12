47978a946d4011a085c2c75b8503acfe3064ec7c
Muchiri-cmd <davismuchiri21@gmail.com>
2025-06-12 16:29:55 +0300

    Add update Task Operation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 //Create Task
 app.post("/tasks", async (req, res) => {
   try {
-    console.log(req.body)
+    console.log(req.body);
     const { title, description } = req.body;
     const newTask = await client.task.create({
       data: { title, description },
@@ -26,35 +26,69 @@ app.post("/tasks", async (req, res) => {
 });
 
 //Get all Tasks
-app.get("/tasks",async(req,res) => {
-    try{
-        const tasks = await client.task.findMany({
-            //get only incomplete tasks
-            where: {
-                isCompleted:false
-            }
-        });
-        res.status(200).json(tasks);
-    }catch( error ){
-        res.status(500).json({ message: "Something went wrong , please try again later." });
-    }
-})
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await client.task.findMany({
+      //get only incomplete tasks
+      where: {
+        isCompleted: false,
+      },
+    });
+    res.status(200).json(tasks);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong , please try again later." });
+  }
+});
 
+//Get 1 Task
 app.get("/tasks/:id", async (req, res) => {
+  try {
+    console.log(req.params);
+    const { id } = req.params;
+    const task = await client.task.findFirst({
+      where: {
+        id, //: id
+      },
+    });
+    task
+      ? res.status(200).json(task)
+      : res.status(404).json({ message: "Task not found." });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching task, please try again later." });
+  }
+});
+
+//Update Task
+app.patch("/tasks/:id", async (req,res) => {
     try{
-        console.log(req.params)
-        const {id} = req.params;
-        const task = await client.task.findFirst({
-            where:
-            {
-                id //: id
+        console.log(req.body);
+        console.log("Req Params:", req.params);
+
+        const { id } = req.params;
+
+        console.log("Req Body:", req.body);
+
+        const { title, description, isCompleted } = req.body;
+
+        const updatedTask = await client.task.update({
+            where: {
+                id
+            },
+            data: {
+                title,
+                description,
+                isCompleted
             }
         })
-        task ? res.status(200).json(task) : res.status(404).json({ message: "Task not found." });
-    }catch(error){
-        res.status(500).json({ message: "Error fetching task, please try again later." });
+        res.status(200).json(updatedTask);
+    } catch(error){
+        console.log(error)
+        res.status(500).json({ message: "Error updating task, please try again later." });
     }
-   
 })
 
 const PORT = process.env.PORT || 3000;
